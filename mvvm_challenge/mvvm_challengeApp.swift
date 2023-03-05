ec3063043ecd95a44d1c81c77c113def2c48a587
ZachMommaerts <zmommaerts@gmail.com>
2023-03-05 00:31:32 -0600

    Initial Commit

---
+++ mvvm_challenge/mvvm_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvm_challengeApp.swift
+//  mvvm_challenge
+//
+//  Created by Zach Mommaerts on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct mvvm_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
